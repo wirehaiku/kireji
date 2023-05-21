47194375496f7e61b9a3ab937a580ae6be4d258e
Stephen Malone <mail@wirehaiku.org>
2023-05-21 14:21:33 +1000

    Added items/book package.

---
+++ kireji/items/book/book.go
@@ -0,0 +1,93 @@
+// Package book implements the Book type and methods.
+package book
+
+import (
+	"fmt"
+	"io/fs"
+
+	"github.com/wirehaiku/kireji/kireji/items/note"
+	"github.com/wirehaiku/kireji/kireji/tools/fsys"
+	"github.com/wirehaiku/kireji/kireji/tools/neat"
+)
+
+// Book is a directory of plaintext Note files.
+type Book struct {
+	Dire string
+	Extn string
+	Mode fs.FileMode
+}
+
+// New returns a new Book.
+func New(dire, extn string, mode fs.FileMode) *Book {
+	return &Book{neat.Path(dire), neat.Extn(extn), mode}
+}
+
+// Create creates and returns a new Note.
+func (b *Book) Create(name, body string) (*note.Note, error) {
+	path := fsys.Join(b.Dire, neat.Name(name), b.Extn)
+	note := note.New(path, b.Mode)
+	if note.Exists() {
+		return nil, fmt.Errorf("note %q already exists", name)
+	}
+
+	if err := note.Write(body); err != nil {
+		return nil, err
+	}
+
+	return note, nil
+}
+
+// Get returns an existing Note by name.
+func (b *Book) Get(name string) (*note.Note, error) {
+	dest := fsys.Join(b.Dire, neat.Name(name), b.Extn)
+	for _, path := range fsys.Glob(b.Dire, b.Extn) {
+		if path == dest {
+			return note.New(path, b.Mode), nil
+		}
+	}
+
+	return nil, fmt.Errorf("note %q does not exist", name)
+}
+
+// Filter returns all Notes passing a filter function.
+func (b *Book) Filter(fun func(*note.Note) (bool, error)) ([]*note.Note, error) {
+	var notes []*note.Note
+	for _, note := range b.List() {
+		ok, err := fun(note)
+		if err != nil {
+			return nil, err
+		}
+
+		if ok {
+			notes = append(notes, note)
+		}
+	}
+
+	return notes, nil
+}
+
+// List returns all Notes in the Book's directory in sorted order.
+func (b *Book) List() []*note.Note {
+	var notes []*note.Note
+	for _, path := range fsys.Glob(b.Dire, b.Extn) {
+		notes = append(notes, note.New(path, b.Mode))
+	}
+
+	return notes
+}
+
+// Match returns all Notes with names containing a substring.
+func (b *Book) Match(sub string) []*note.Note {
+	notes, _ := b.Filter(func(note *note.Note) (bool, error) {
+		return note.Match(sub), nil
+	})
+
+	return notes
+}
+
+// Search returns all Notes with bodies containing a substring.
+func (b *Book) Search(sub string) ([]*note.Note, error) {
+	return b.Filter(func(note *note.Note) (bool, error) {
+		return note.Search(sub)
+	})
+}
---
+++ kireji/items/book/book_test.go
@@ -0,0 +1,103 @@
+package book
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wirehaiku/kireji/kireji/items/note"
+	"github.com/wirehaiku/kireji/kireji/tools/test"
+)
+
+func testBook(t *testing.T) *Book {
+	dire := test.Dire(t)
+	return New(dire, ".txt", 0666)
+}
+
+func TestNew(t *testing.T) {
+	// success
+	book := testBook(t)
+	assert.NotEmpty(t, book.Dire)
+	assert.Equal(t, ".txt", book.Extn)
+	assert.Equal(t, fs.FileMode(0666), book.Mode)
+}
+
+func TestCreate(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	note, err := book.Create("test", "test\n")
+	bits, _ := os.ReadFile(note.Path)
+	assert.Equal(t, filepath.Join(book.Dire, "test.txt"), note.Path)
+	assert.Equal(t, fs.FileMode(0666), note.Mode)
+	assert.Equal(t, "test\n", string(bits))
+	assert.NoError(t, err)
+
+	// failure - existing Note
+	note, err = book.Create("alpha", "")
+	assert.Nil(t, note)
+	assert.EqualError(t, err, `note "alpha" already exists`)
+}
+
+func TestGet(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	note, err := book.Get("alpha")
+	assert.Equal(t, filepath.Join(book.Dire, "alpha.txt"), note.Path)
+	assert.NoError(t, err)
+
+	// failure - nonexistent note
+	note, err = book.Get("nope")
+	assert.Nil(t, note)
+	assert.EqualError(t, err, `note "nope" does not exist`)
+}
+
+func TestFilter(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	notes, err := book.Filter(func(note *note.Note) (bool, error) {
+		return note.Name() == "alpha", nil
+	})
+	assert.Len(t, notes, 1)
+	assert.Equal(t, filepath.Join(book.Dire, "alpha.txt"), notes[0].Path)
+	assert.NoError(t, err)
+}
+
+func TestList(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	notes := book.List()
+	assert.Len(t, notes, 2)
+	assert.Equal(t, filepath.Join(book.Dire, "alpha.txt"), notes[0].Path)
+	assert.Equal(t, filepath.Join(book.Dire, "bravo.txt"), notes[1].Path)
+}
+
+func TestMatch(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	notes := book.Match("alph")
+	assert.Len(t, notes, 1)
+	assert.Equal(t, filepath.Join(book.Dire, "alpha.txt"), notes[0].Path)
+}
+
+func TestSearch(t *testing.T) {
+	// setup
+	book := testBook(t)
+
+	// success
+	notes, err := book.Search("alph")
+	assert.Len(t, notes, 1)
+	assert.Equal(t, filepath.Join(book.Dire, "alpha.txt"), notes[0].Path)
+	assert.NoError(t, err)
+}
